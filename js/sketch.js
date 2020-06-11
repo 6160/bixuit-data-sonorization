36c90ad230e11414be221fc280dd6603a237537d
6160 <marco@sixonesixo.com>
2020-06-11 15:10:53 +0200

    now UI constants are kinda easier to manage

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,8 @@
-const graphX = 230;
-const lineL = 25 + graphX;
-const lineR = 30 + graphX;
-const graphLineX = lineR + 10;
-const graphLineY = {}
-const paddingGraphText = 16;
-const paddingGraphLine = 25;
+// constants
+const MINWIDTH = 400;
+const UI = {};
 
+// other stuff
 let song;
 let amp;
 let button;
@@ -24,26 +21,96 @@ function toggleSong() {
 function preload() {
     console.log(' #### loading assets.')
     song = loadSound('./audio/GLADOS.mp3');
-    // bgimg = loadImage('./img/bg.png');
 }
+
+function assignPositions() {
+    if (width < MINWIDTH) {
+        // mobile
+        UI.graphStartX = 5;
+        UI.graphUI = {};
+        UI.graphUI.indicator = {
+            start: 25 + UI.graphStartX,
+            end: 30 + UI.graphStartX,
+        };
+
+        UI.graphUI.line = {
+            x: UI.graphUI.indicator.end + 10,
+            y: {
+                bottom: height - 25,
+                mid: height - 100,
+                high: height - 175,
+            }
+        };
+
+        UI.paddingGraphLine = 25;
+        UI.paddingGraphText = 16
+        UI.ellipse = {
+            x: width/2,
+            y: height - 175 - 100,
+        };
+
+        UI.volstring = {
+            x: 12,
+            y: UI.ellipse.y - 50,
+        }
+
+        UI.sampleNo = {
+            x: width - 20,
+            y: UI.ellipse.y + 50,
+        }
+    } else {
+        // desktop
+        UI.graphStartX = 230;
+        UI.graphUI = {};
+        UI.graphUI.indicator = {
+            start: 25 + UI.graphStartX,
+            end: 30 + UI.graphStartX,
+        };
+
+        UI.graphUI.line = {
+            x: UI.graphUI.indicator.end + 10,
+            y: {
+                bottom: height - 25,
+                mid: height - 100,
+                high: height - 175,
+            }
+        };
+
+        UI.paddingGraphLine = 25;
+        UI.paddingGraphText = 16
+        UI.ellipse = {
+            x: 125,
+            y: height - 100,
+        };
+
+        UI.volstring = {
+            x: 20,
+            y: height - 25,
+        }
+
+        UI.sampleNo = {
+            x: 20,
+            y: height - 175,
+        }
+    }
+}
+
 
 function setup() {
     console.log(' #### loading variables.')
-    console.log(' #### > dpr ',window.devicePixelRatio)
+    console.log(' #### > dpr ', window.devicePixelRatio)
     pixelDensity(window.devicePixelRatio)
     console.log(' #### > dim ', window.innerWidth, window.innerHeight)
-    
+
     let c = createCanvas(window.innerWidth, window.innerHeight);
     c.parent("p5canvas");
-    
+
     // button = createButton('toggle');
     // button.mousePressed(toggleSong);
-    
+
     song.play();
     amp = new p5.Amplitude();
-    graphLineY.bottom = height - 25;
-    graphLineY.mid = height - 100;
-    graphLineY.high = height - 175;
+    assignPositions();
     console.log(' #### sending start message.')
     window.postMessage('START', '*');
 }
@@ -51,44 +118,31 @@ function setup() {
 function drawUI() {
     textFont('Monospace');
     textSize(8);
-    line(lineL, graphLineY.bottom, lineR, graphLineY.bottom)
-    line(lineL, graphLineY.mid, lineR, graphLineY.mid)
-    line(lineL, graphLineY.high, lineR, graphLineY.high)
+    line(UI.graphUI.indicator.start, UI.graphUI.line.y.bottom, UI.graphUI.indicator.end, UI.graphUI.line.y.bottom)
+    line(UI.graphUI.indicator.start, UI.graphUI.line.y.mid, UI.graphUI.indicator.end, UI.graphUI.line.y.mid)
+    line(UI.graphUI.indicator.start, UI.graphUI.line.y.high, UI.graphUI.indicator.end, UI.graphUI.line.y.high)
 
-    text('0.0',lineL - paddingGraphText, graphLineY.bottom)
-    text('0.5',lineL - paddingGraphText, graphLineY.mid)
-    text('1.0',lineL - paddingGraphText, graphLineY.high)
+    text('0.0', UI.graphUI.indicator.start - UI.paddingGraphText, UI.graphUI.line.y.bottom)
+    text('0.5', UI.graphUI.indicator.start - UI.paddingGraphText, UI.graphUI.line.y.mid)
+    text('1.0', UI.graphUI.indicator.start - UI.paddingGraphText, UI.graphUI.line.y.high)
 
     stroke('rgba(255,255,255,0.2)');
-    line(lineR, graphLineY.bottom, width - paddingGraphLine, graphLineY.bottom)
-    line(lineR, graphLineY.mid, width - paddingGraphLine, graphLineY.mid)
-    line(lineR, graphLineY.high, width - paddingGraphLine, graphLineY.high)
+    line(UI.graphUI.indicator.end, UI.graphUI.line.y.bottom, width - UI.paddingGraphLine, UI.graphUI.line.y.bottom)
+    line(UI.graphUI.indicator.end, UI.graphUI.line.y.mid, width - UI.paddingGraphLine, UI.graphUI.line.y.mid)
+    line(UI.graphUI.indicator.end, UI.graphUI.line.y.high, width - UI.paddingGraphLine, UI.graphUI.line.y.high)
 }
 
-function bgTile() {
-    const tile = 50;
-    const x_tile = width / tile;
-    const y_tile = height / tile;
-    
-    stroke('rgba(255,255,255,0.01)');
-    noFill();
-    for (let i = 0; i < x_tile; i ++) {
 
-        for (let j = 0; j < y_tile; j ++) {
-            square(i * tile, j * tile, tile);
-        }
-    }
-}
 
 function draw() {
     clear();
     // background(37,50,104);
     // image(bgimg, 0, 0, width, height)
     drawUI();
-    
+
     var vol = amp.getLevel();
     volhistory.push(vol);
-    sampleNo+=1;
+    sampleNo += 1;
     stroke(255);
     noFill();
     push();
@@ -96,19 +150,20 @@ function draw() {
     beginShape();
     for (var i = 0; i < volhistory.length; i++) {
         var y = map(volhistory[i], 0, 1, height - 25, height - 500);
-        vertex((i*4)+ graphLineX, y);
+        vertex((i * 4) + UI.graphUI.line.x, y);
     }
     endShape();
     pop();
 
-    if (volhistory.length * 4 > (width) - 50 - graphLineX) {
+    if (volhistory.length * 4 > (width) - 50 - UI.graphUI.line.x) {
         volhistory = [];
     }
 
-    ellipse(125, height - 100, 200, vol * 200);
+    ellipse(UI.ellipse.x, UI.ellipse.y, 200, vol * 200);
     textFont('monospace');
     textSize(8);
+
     let volstring = vol.toString()
-    text(volstring.substring(0,4), 20, height - 25);
-    text(sampleNo, 20, height - 175)
+    text(volstring.substring(0, 4), UI.volstring.x, UI.volstring.y);
+    text(sampleNo, UI.sampleNo.x, UI.sampleNo.y);
 }
